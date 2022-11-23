18f8eb6b619f312080e130dfd4558d698c9444be
Stiflerzak <hilarymwendia97@gmail.com>
2022-11-23 15:55:13 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1=2
+let num2=31
+let multiply= num1*num2
+
+let random=1
+
+let num3= 16
+let num4=6
+let mod= num3%num4
+let max= 20
+
+
+
+// function multiply(${num1}*${num2}){
+//    console.log(num1*num2===62); 
+// }
+// multiply(2,31)
+
